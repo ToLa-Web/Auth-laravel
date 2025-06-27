f31ad511cce80da6b844d9377ce875d78ebebd55
kirato01 <chhengtola4@gmail.com>
2025-06-27 21:13:52 +0700

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        $user_id = auth()->user()->id;
+
+        $products = Product::where("user_id", $user_id)->get();
+
+        return response()->json([
+            "status" => true,
+            "products" => $products
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            "title" => "required"
+        ]);
+
+        $data["user_id"] = auth()->user()->id;
+        if ($request->hasFile("banner_image")) {
+            $data["banner_image"] = $request->file("banner_image")->store("products", "public");
+        }
+
+        Product::create($data);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product created successfully"
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return response()->json([
+            "status" => true,
+            "message" => "Product data retrieved successfully",
+            "product" => $product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $data = $request->validate([
+            "title" => "required",
+        ]);
+
+        if ($request->hasFile("banner_image")) {
+            if ($product->banner_image) {
+                Storage::disk("public")->delete($product->banner_image);
+            }
+
+            $data["banner_image"] = $request->file("banner_image")->store("products", "public");
+        }
+
+        $product->update($data);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product data updated successfully"
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Product deleted successfully"
+        ]);
+    }
+}
